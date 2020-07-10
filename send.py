123bfc570f1c0c56ae8e3e5358c2839c86adf77a
Faraj <alhaponyfarag@gmail.com>
2020-07-10 20:04:26 +0200

    defining connection parameters in send client

--- send.py
+++ send.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 import pika
 import sys
+
+credentials = pika.PlainCredentials('admin', 'admin')
+connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='192.168.0.121', credentials=credentials))
+channel = connection.channel()
+# connect to the middleware
