d7ba66e23380be7968543fa0c700b530ee123d90
Faraj <alhaponyfarag@gmail.com>
2020-07-10 19:32:00 +0200

    Adding imports to app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import kivy
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.scrollview import ScrollView
+from kivy.clock import Clock
+import send
+import os
+import sys
+
+# The used version of kivy
+kivy.require("1.11.1")
