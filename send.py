3bdf16ea208b5fc79aac946f30bd198196ba83fb
Faraj <alhaponyfarag@gmail.com>
2020-07-10 20:04:59 +0200

    Adding send function to send client

--- send.py
+++ send.py
@@ -7,3 +7,11 @@ connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='192.168.0.121', credentials=credentials))
 channel = connection.channel()
 # connect to the middleware
+
+def send(username, message):
+
+    channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+    message = ' '.join(sys.argv[1:]) or "info:" + message + "!"
+    channel.basic_publish(exchange='logs', routing_key='', body=message)
+    print(" Sent " + message + " !" + "from "+ username)
