75a696a2031cd83fc130a478ca1a84b9c752646e
Faraj <alhaponyfarag@gmail.com>
2020-07-10 20:06:48 +0200

    defining connection parameters to receive server

--- recieve.py
+++ recieve.py
@@ -3,3 +3,17 @@ from time import sleep
 
 import pika
 import send
+
+credentials = pika.PlainCredentials('admin', 'admin')
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='192.168.0.121',credentials=credentials))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(exchange='logs', queue=queue_name)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
